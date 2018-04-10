06a7841ea08c3ff255f0e4df041e9781755eda4d
Geoff Goldrick <geoff.goldrick@det.nsw.edu.au>
2018-04-10 13:44:24 +1000

    Add files via upload

---
+++ short header.py
@@ -0,0 +1,23 @@
+""" A simple game that moves a sprite with your mouse.
+
+An introduction to python and ghow to set up the environments,
+load a spriteand move it with a mouse
+"""
+
+__author__ = "De G"
+__copyright__ = "Copyright 2018, NSW Dept. Ed."
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Geoff Goldrick"
+__email__ = "geoff.goldrick@education.nsw.com.au"
+__status__ = "Prototype"
+
+#dendencies
+import pygame
+
+#set global variables
+displaySize = [800,600]
+
+#create game window
+mainWindow = pygame.display.set_mode(displaySize)
+
